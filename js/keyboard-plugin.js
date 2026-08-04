9d2c8199f2182fdf1ebf58f84d24373b1b66ab59
kolman666 <dickius48282@gmail.com>
2026-08-04 14:56:15 +0300

    refactor: добавлен плагин для логики клавиатуры

---
+++ js/keyboard-plugin.js
@@ -0,0 +1,43 @@
+(function () {
+    class KeyboardPlugin {
+        constructor(target) {
+            this._pressedKeys = new Set();
+            this._onKeyDown = this._onKeyDown.bind(this);
+            this._onKeyUp = this._onKeyUp.bind(this);
+
+            if (target) {
+                this.attach(target);
+            }
+        }
+
+        attach(target) {
+            this._target = target;
+            target.addEventListener("keydown", this._onKeyDown);
+            target.addEventListener("keyup", this._onKeyUp);
+        }
+
+        detach() {
+            if (!this._target) return;
+
+            this._target.removeEventListener("keydown", this._onKeyDown);
+            this._target.removeEventListener("keyup", this._onKeyUp);
+
+            this._pressedKeys.clear();
+            this._target = null;
+        }
+
+        _onKeyDown(e) {
+            this._pressedKeys.add(e.keyCode);
+        }
+
+        _onKeyUp(e) {
+            this._pressedKeys.delete(e.keyCode);
+        }
+
+        isActionActive(actionConfig) {
+            if (!actionConfig.keys) return false;
+            return actionConfig.keys.some(key => this._pressedKeys.has(key));
+        }
+    }
+    window.KeyboardPlugin = KeyboardPlugin;
+})();
